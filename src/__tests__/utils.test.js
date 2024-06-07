40c1102a4f1cb45015e11cbdd1c17323066679a3
Buddha Lo <lo_977@yahoo.com>
2024-06-06 22:29:29 -0700

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+describe("pointsForWord", () => {
+  //   it("calculates the total points for a word(1 point per vowel, 2 for consonant", () => {
+  it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+    const points = pointsForWord(word);
+    expect(points).toBe(7);
+  });
+});
